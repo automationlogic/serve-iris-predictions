6d71a8c20ac0ce9bf759f381fa17fe87df51a168
Maartens <maartens@automationlogic.com>
2020-05-15 15:49:26 +0100

    Fetch data from bigquery rather than corpus; calculate and display accuracy

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 from google.cloud import storage
+from google.cloud import bigquery
 from sklearn import datasets
 from joblib import load
+import numpy as np
 
 import shutil
 import os
@@ -13,10 +15,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    print("Making predictions..")
-    predictions = f"{model.predict(X)}"
-    real_targets = f"{y}"
-    return render_template('home.html', predictions=predictions, real_targets=real_targets)
+    print("Making predictions ...")
+    predictions = f"{model.predict(iris_features)}"
+    real_targets = f"{iris_names}"
+    accuracy = np.mean(predictions == real_targets)
+    return render_template('home.html', predictions=predictions, real_targets=real_targets, accuracy=accuracy)
 
 @app.errorhandler(500)
 def server_error(e):
@@ -26,9 +29,21 @@ def server_error(e):
 print("Preparing..")
 model_name = os.getenv('MODEL_NAME')
 
-print("Downloading iris dataset..")
-iris = datasets.load_iris()
-X, y = iris.data, iris.target
+print("Fetching data from BigQuery ...")
+client = bigquery.Client()
+
+query = (
+    "SELECT * FROM `" + os.environ['PROJECT_ID'] + ".iris.iris`"
+)
+query_job = client.query(
+    query,
+)
+
+iris_features = []
+iris_names = []
+for row in query_job:
+    iris_features.append([row.Sepal_Length,row.Sepal_Width,row.Petal_Length,row.Petal_Width])
+    iris_names.append(row.Flower_Type)
 
 print("Loading model..")
 model = load(model_name)
